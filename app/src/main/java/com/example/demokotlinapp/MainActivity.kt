509dbfebb8f60f17badb40b0691a74a0e81bb655
Khushi <exotica.xo1@gmail.com>
2023-06-19 16:31:34 +0530

    project files updated

--- app/src/main/java/com/example/demokotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/demokotlinapp/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.demokotlinapp
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintSet.Constraint
 
@@ -56,14 +61,40 @@ class MainActivity : AppCompatActivity() {
         val camerabtn=findViewById<CardView>(R.id.cameraView)
         val browserBtn=findViewById<Button>(R.id.webBtn)
         val link=findViewById<EditText>(R.id.browseLink)
+        val msgBtn=findViewById<CardView>(R.id.messageView)
+        val emailBtn=findViewById<CardView>(R.id.emailView)
 
         browserBtn.setOnClickListener(View.OnClickListener {
             val intents=Intent(Intent.ACTION_VIEW)
-           // val urlText=link.editableText.toString()
-            intents.data= Uri.parse("https://www.google.com")
+            val urlText=link.text.toString()
+            intents.data= Uri.parse(urlText)
             startActivity(intents)
         })
 
+        camerabtn.setOnClickListener(View.OnClickListener {
+            val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+
+        })
+
+        msgBtn.setOnClickListener(View.OnClickListener {
+            val intent=Intent(Intent.ACTION_CALL)
+            startActivity(intent)
+
+        })
+
+        val webViewVariable=findViewById<WebView>(R.id.webView)
+        webViewSetup(webViewVariable)
+    }
+
+    private fun webViewSetup(webView: WebView){
+        webView.webViewClient= WebViewClient()
+        webView.apply {
+            loadUrl("https://www.github.com/SmileWithKhushi")
+            settings.javaScriptEnabled=true
+
+
+        }
 
 
     }
