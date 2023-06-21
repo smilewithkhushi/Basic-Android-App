bda7e0d552634623d98b1a4be2769b1b51c90476
Khushi <exotica.xo1@gmail.com>
2023-06-21 23:34:45 +0530

    added intents and data exchange

--- app/src/main/java/com/example/demokotlinapp/screen2.kt
+++ app/src/main/java/com/example/demokotlinapp/screen2.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class screen2 : AppCompatActivity() {
@@ -12,14 +13,21 @@ class screen2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen2)
 
+        //button to move to home screen
         val screenbtn1=findViewById<Button>(R.id.firstScreenBtn)
-
         screenbtn1.setOnClickListener(
             View.OnClickListener {
                 Toast.makeText(applicationContext, "Moving to home screen..", Toast.LENGTH_SHORT).show()
-            intent=Intent(applicationContext, MainActivity::class.java)
+                intent=Intent(applicationContext, MainActivity::class.java)
                 startActivity(intent)
             }
         )
+
+        //receving data from first screen and showing the data in the text view of second screen
+        val messageFromPerson=intent.getStringExtra(MainActivity.KEY)
+        val personTextArea=findViewById<TextView>(R.id.personDetails)
+        personTextArea.text=messageFromPerson
+
+
     }
 }
